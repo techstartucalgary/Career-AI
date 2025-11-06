94bafa4e3969f3224a377302fe95d811dd01d5d6
wugawrld <tareenwaheed321@gmail.com>
2025-11-05 21:06:32 -0700

    added header and footer components, home page; the changes i did made regarding the router will be done later when we have the rest of the pages figured out

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,9 @@
 import React from 'react'
 import './App.css'
 import HomePage from './pages/HomePage.jsx'
-import { Routes, Route } from 'react-router-dom'
-
+/* deleted the router FOR NOW (we can add this after we have the rest of the pages figured out) */
 const App = () => {
-
-  return (
-      <div>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-          </Routes>
-      </div>
-  )
+  return <HomePage />
 }
 
 export default App
