fab2c5b901a072290ad3ad45e0918e5e82894723
Abdulrahman Negmeldin <abdulrahman.m.negmeldin@gmail.com>
2025-12-15 18:09:01 -0700

    finish signup endpoint

--- backend/backend.py
+++ backend/backend.py
@@ -34,6 +34,7 @@ database = os.environ.get("DATABASE")
 clientdb = pymongo.MongoClient(f"{database}")
 db_info = os.environ.get("DATABASE_INFO")
 db = clientdb[f'{db_info}']
+col = db["users"]
 
 """
 Helper function to hash passwords for security purposes
@@ -51,6 +52,21 @@ def verify_hash(stored_pwd, provided_pwd):
     stored_pwd = stored_pwd.encode("utf-8")
 
     return bcrypt.checkpw(provided_pwd.encode("utf-8"), stored_pwd)
+
+"""
+Helper function to create a JWT access token for the given user ID
+"""
+def create_access_token(userID: int, role: str) -> str:
+
+    now = datetime.now(timezone.utc)
+    payload = {
+        "sub": str(userID),  # subject
+        "iat": int(now.timestamp()),
+        "exp": int((now + timedelta(minutes=jwt_expire_minutes)).timestamp()),
+        "role": role,
+    }
+
+    return jwt.encode(payload, algorithm=jwt_algo)
 
 @app.get("/login")
 def login():
@@ -106,6 +122,49 @@ async def signup(request: Request):
     hashed_linkedin = hash_(linkedin)
     hashed_github = hash_(github)
     hashed_location = hash_(location)
+    reg_date = datetime.utcnow().date().isoformat()  # date of account creation (now)
+    role = "free_user"
+
+    try:
+        user = col.find({"email": email})
+        if user:
+            return JSONResponse(
+                status_code=400,
+                content={
+                    "success": False,
+                    "message": "Email already in use!"
+                }
+            )
+    except:
+        pass
+
+    try:
+        dic = {"email":email, "password":hashed_pwd, "first_name": hashed_fname, "last_name": hashed_lname, "phone": hashed_phone,
+           "linkedin": hashed_linkedin, "github": hashed_github, "location": hashed_location, "registration_date": reg_date, "role": role}
+        user = col.insert_one(dic)
+        user_id = user.inserted_id
+        access_token = create_access_token(user_id, role)
+        return JSONResponse(
+            status_code=201,
+            content={
+                "success": True,
+                "message": "Account created successfully.",
+                "data": {
+                    "user_id": user_id,
+                    "email": email,
+                    "role": role,
+                    "token": access_token
+                }
+            }
+        )
+    except:
+        return JSONResponse(
+            status_code=400,
+            content={
+                "success": False,
+                "message": "Server side error!"
+            }
+        )
 
 
 
